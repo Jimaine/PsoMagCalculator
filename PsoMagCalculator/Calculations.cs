a1fc539f19412f25d9d8a87fa6e0d9c99e14d375
Jimaine <mikegrnewald@yahoo.de>
2014-10-15 10:34:25 +0200

    Update Calculations.cs

--- PsoMagCalculator/Calculations.cs
+++ PsoMagCalculator/Calculations.cs
@@ -14,9 +14,9 @@ namespace PsoMagCalculator
     /// Modifies the values of the Main Mag.
     /// </summary>
     /// <param name="p_mag">The Mag to modify.</param>
-    /// <param name="p_item">The Item to feed the Mag</param>
-    /// <param name="p_character">The character who feed the Mag</param>
-    /// <param name="p_sectionId">The Id of the character who feed the Mag</param>
+    /// <param name="p_item">The Item to feed the Mag.</param>
+    /// <param name="p_character">The character who feed the Mag.</param>
+    /// <param name="p_sectionId">The Id of the character who feed the Mag.</param>
     public static void Recalculate(Mag p_mag, Item p_item, Character p_character, SectionId p_sectionId)
     {
       SetMag(p_mag, p_item);
@@ -29,8 +29,8 @@ namespace PsoMagCalculator
     /// <summary>
     /// Return the Button's Tooltip as a string.
     /// </summary>
-    /// <param name="p_item">The Item which represents the button</param>
-    /// <param name="p_magName">The Name of the Main Mag</param>
+    /// <param name="p_item">The Item which represents the button.</param>
+    /// <param name="p_magName">The Name of the Main Mag.</param>
     /// <returns>string</returns>
     public static string GetTooltip(Item p_item, MagName p_magName)
     {
@@ -140,13 +140,11 @@ namespace PsoMagCalculator
     {
       int levelUps = SetLevel(p_mag);
 
-      // Mag Change only for non-epic
+      // Mag Change only for non-epic.
       if (p_mag.Name != MagName.Rukmin && p_mag.Name != MagName.Bhima && p_mag.Name != MagName.Nidra && p_mag.Name != MagName.Rukmin && p_mag.Name != MagName.Pushan && p_mag.Name != MagName.Savitri && p_mag.Name != MagName.Rati && p_mag.Name != MagName.Sato && p_mag.Name != MagName.Deva && p_mag.Name != MagName.Diwari)
         if (levelUps >= 1)
           for (int i = 0; i < levelUps; i++)
-          {
             CheckMagChange(p_mag, p_character, p_sectionId);
-          }
     }
 
     private static int SetLevel(Mag p_mag)
@@ -199,23 +197,23 @@ namespace PsoMagCalculator
         case 12:
         case 13:
           if (p_mag.Name == MagName.Mag)
-            MagChangeOne(p_mag, p_character);
+            FirstMagChange(p_mag, p_character);
           break;
         case 35:
         case 36:
         case 37:
         case 38:
           if (p_mag.Name == MagName.Varuna || p_mag.Name == MagName.Kalki || p_mag.Name == MagName.Vritra)
-            MagChangeTwo(p_mag, p_character);
+            SecondMagChange(p_mag, p_character);
           break;
         default:
           if (p_mag.Level >= 50 && (p_mag.Level % 5) == 0)
-            MagChangeThree(p_mag, p_character, p_sectionId);
+            EndMagChange(p_mag, p_character, p_sectionId);
           break;
       }
     }
 
-    private static void MagChangeOne(Mag p_mag, Character p_character)
+    private static void FirstMagChange(Mag p_mag, Character p_character)
     {
       switch (p_character)
       {
@@ -237,7 +235,7 @@ namespace PsoMagCalculator
       }
     }
 
-    private static void MagChangeTwo(Mag p_mag, Character p_character)
+    private static void SecondMagChange(Mag p_mag, Character p_character)
     {
       switch (p_character)
       {
@@ -313,7 +311,7 @@ namespace PsoMagCalculator
       }
     }
 
-    private static void MagChangeThree(Mag p_mag, Character p_character, SectionId p_sectionId)
+    private static void EndMagChange(Mag p_mag, Character p_character, SectionId p_sectionId)
     {
       switch (p_sectionId)
       {
@@ -328,7 +326,7 @@ namespace PsoMagCalculator
             case Character.HunterFemale:
               if (p_mag.PowLevel > p_mag.DexLevel && p_mag.PowLevel > p_mag.MindLevel)
               {
-                // POW is First, look for second
+                // POW is First, look for second.
                 if (p_mag.DexLevel >= p_mag.MindLevel)
                   p_mag.Name = MagName.Varaha;
                 else
@@ -336,7 +334,7 @@ namespace PsoMagCalculator
               }
               else if (p_mag.DexLevel > p_mag.PowLevel && p_mag.DexLevel > p_mag.MindLevel)
               {
-                // Dex is First, look for second
+                // Dex is First, look for second.
                 if (p_mag.MindLevel >= p_mag.PowLevel)
                   p_mag.Name = MagName.Nandin;
                 else
@@ -344,7 +342,7 @@ namespace PsoMagCalculator
               }
               else if (p_mag.MindLevel > p_mag.PowLevel && p_mag.MindLevel > p_mag.DexLevel)
               {
-                // Mind is First, look for second
+                // Mind is First, look for second.
                 if (p_mag.PowLevel >= p_mag.DexLevel)
                   p_mag.Name = MagName.Kabanda;
                 else
@@ -352,7 +350,7 @@ namespace PsoMagCalculator
               }
               else
               {
-                // Tie of Pow/Dex/Mind, look for second
+                // Tie of Pow/Dex/Mind, look for second.
                 if (p_mag.DexLevel >= p_mag.MindLevel)
                   p_mag.Name = MagName.Varaha;
                 else
@@ -395,7 +393,7 @@ namespace PsoMagCalculator
               if (p_mag.PowLevel > p_mag.DexLevel && p_mag.PowLevel > p_mag.MindLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Andhaka; // special def mag for Force
+                  p_mag.Name = MagName.Andhaka; // special def mag for Force.
                 else
                   if (p_mag.DexLevel >= p_mag.MindLevel)
                     p_mag.Name = MagName.Naraka;
@@ -405,7 +403,7 @@ namespace PsoMagCalculator
               else if (p_mag.DexLevel > p_mag.PowLevel && p_mag.DexLevel > p_mag.MindLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force
+                  p_mag.Name = MagName.Bana; // special def mag for Force.
                 else
                   if (p_mag.MindLevel >= p_mag.PowLevel)
                     p_mag.Name = MagName.Sita;
@@ -415,7 +413,7 @@ namespace PsoMagCalculator
               else if (p_mag.MindLevel > p_mag.PowLevel && p_mag.MindLevel > p_mag.DexLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force
+                  p_mag.Name = MagName.Bana; // special def mag for Force.
                 else
                   if (p_mag.PowLevel >= p_mag.DexLevel)
                     p_mag.Name = MagName.Naga;
@@ -425,7 +423,7 @@ namespace PsoMagCalculator
               else
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force
+                  p_mag.Name = MagName.Bana; // special def mag for Force.
                 else
                   if (p_mag.PowLevel >= p_mag.DexLevel)
                     p_mag.Name = MagName.Naga;
@@ -448,7 +446,7 @@ namespace PsoMagCalculator
             case Character.HunterFemale:
               if (p_mag.PowLevel > p_mag.DexLevel && p_mag.PowLevel > p_mag.MindLevel)
               {
-                // POW is First, look for second
+                // POW is First, look for second.
                 if (p_mag.DexLevel >= p_mag.MindLevel)
                   p_mag.Name = MagName.Kama;
                 else
@@ -456,7 +454,7 @@ namespace PsoMagCalculator
               }
               else if (p_mag.DexLevel > p_mag.PowLevel && p_mag.DexLevel > p_mag.MindLevel)
               {
-                // Dex is First, look for second
+                // Dex is First, look for second.
                 if (p_mag.MindLevel >= p_mag.PowLevel)
                   p_mag.Name = MagName.Yaksa;
                 else
@@ -464,7 +462,7 @@ namespace PsoMagCalculator
               }
               else if (p_mag.MindLevel > p_mag.PowLevel && p_mag.MindLevel > p_mag.DexLevel)
               {
-                // Mind is First, look for second
+                // Mind is First, look for second.
                 if (p_mag.PowLevel >= p_mag.DexLevel)
                   p_mag.Name = MagName.Bana;
                 else
@@ -472,7 +470,7 @@ namespace PsoMagCalculator
               }
               else
               {
-                // Tie of Pow/Dex/Mind, look for second
+                // Tie of Pow/Dex/Mind, look for second.
                 if (p_mag.DexLevel >= p_mag.MindLevel)
                   p_mag.Name = MagName.Kama;
                 else
@@ -515,7 +513,7 @@ namespace PsoMagCalculator
               if (p_mag.PowLevel > p_mag.DexLevel && p_mag.PowLevel > p_mag.MindLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Andhaka; // special def mag for Force. Perhaps Pow > Dex + Mind
+                  p_mag.Name = MagName.Andhaka; // special def mag for Force. Perhaps Pow > Dex + Mind.
                 else
                   if (p_mag.DexLevel >= p_mag.MindLevel)
                     p_mag.Name = MagName.Marica;
@@ -525,7 +523,7 @@ namespace PsoMagCalculator
               else if (p_mag.DexLevel > p_mag.PowLevel && p_mag.DexLevel > p_mag.MindLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Dex > Pow + Mind
+                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Dex > Pow + Mind.
                 else
                   if (p_mag.MindLevel >= p_mag.PowLevel)
                     p_mag.Name = MagName.Bhirava;
@@ -535,7 +533,7 @@ namespace PsoMagCalculator
               else if (p_mag.MindLevel > p_mag.PowLevel && p_mag.MindLevel > p_mag.DexLevel)
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Mind > Pow + Dex
+                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Mind > Pow + Dex.
                 else
                   if (p_mag.PowLevel >= p_mag.DexLevel)
                     p_mag.Name = MagName.Kumara;
@@ -545,7 +543,7 @@ namespace PsoMagCalculator
               else
               {
                 if (p_mag.DefLevel >= 45)
-                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Mind > Pow + Dex
+                  p_mag.Name = MagName.Bana; // special def mag for Force. Perhaps Mind > Pow + Dex.
                 else
                   if (p_mag.PowLevel >= p_mag.DexLevel)
                     p_mag.Name = MagName.Kumara;
